df4776c6cf295f47593a00d5fc89e984e9ae5a93
Francisco <Gimli22@gmail.com>
2023-05-07 04:13:15 +0200

    changes css

--- Js/index.js
+++ Js/index.js
@@ -2,91 +2,76 @@ const requestUrl = "./json/data.json";
 
 //Precios Base e Impuestos
 
-let base = 0
-let KingTax = 0
-let CityTax = 0
-let City = ""; 
+let base = 0;
+let KingTax = 0;
+let CityTax = 0;
+let City = "";
 let Nodes = 0;
-let Taxes = 0 ;
-let NodeMovement = 0 ;
-
+let Taxes = 0;
+let NodeMovement = 0;
 
 //Goberment
 
 let gold = 5;
 let energy = 56;
 let glut = 20;
-let goldEnergy = (energy / gold);
-let glutEnergy = (energy / glut);
-
+let goldEnergy = energy / gold;
+let glutEnergy = energy / glut;
 
 //WorkEnergy y oro ganado por trabajo
 
-const WorkEnergy = new Map ([
-
-  ["Cooker", 12 ],
-  ["Blacksmith" , 12],
-  ["Breeder" , 48],
-  ["Carpenter" , 12],
-  ["Farmer" , 72],
-  ["Herbalist" , 12],
-  ["Potter" , 12],
-  ["Tailor" , 12],
-  ["Collector" , 24],
-
+const WorkEnergy = new Map([
+  ["Cooker", 12],
+  ["Blacksmith", 12],
+  ["Breeder", 48],
+  ["Carpenter", 12],
+  ["Farmer", 72],
+  ["Herbalist", 12],
+  ["Potter", 12],
+  ["Tailor", 12],
+  ["Collector", 24],
 ]);
 
-cookerEnergy = (WorkEnergy.get("Cooker") / goldEnergy); 
-blacksmithEnergy = (WorkEnergy.get("Blacksmith") / goldEnergy);
-breederEnergy = (WorkEnergy.get("Breeder") / goldEnergy);
-carpenterEnergy = (WorkEnergy.get("Carpenter") / goldEnergy);
-farmerEnergy = (WorkEnergy.get("Farmer") / goldEnergy);
-herbalistEnergy = (WorkEnergy.get("Herbalist") / goldEnergy);
-potterEnergy = (WorkEnergy.get("Potter") / goldEnergy);
-tailorEnergy = (WorkEnergy.get("Tailor") / goldEnergy);
-
-
-
+cookerEnergy = WorkEnergy.get("Cooker") / goldEnergy;
+blacksmithEnergy = WorkEnergy.get("Blacksmith") / goldEnergy;
+breederEnergy = WorkEnergy.get("Breeder") / goldEnergy;
+carpenterEnergy = WorkEnergy.get("Carpenter") / goldEnergy;
+farmerEnergy = WorkEnergy.get("Farmer") / goldEnergy;
+herbalistEnergy = WorkEnergy.get("Herbalist") / goldEnergy;
+potterEnergy = WorkEnergy.get("Potter") / goldEnergy;
+tailorEnergy = WorkEnergy.get("Tailor") / goldEnergy;
 
 async function fetchdata() {
   const response = await fetch(requestUrl);
   const data = await response.json();
   return data;
-
 }
 
-function finalPrice(Price){
-  
-    
-    nodeMove = (Nodes * NodeMovement)/100;
-    return Price*base*(1+(Taxes/100)+(nodeMove)+(cookerEnergy))  
+function finalPrice(Price) {
+  nodeMove = (Nodes * NodeMovement) / 100;
+  return Price * base * (1 + Taxes / 100 + nodeMove + cookerEnergy);
+}
 
-}   
-
-
-const cities = new Map()
+const cities = new Map();
 
 fetchdata().then((data) => {
-
-    for (let index = 0; index < data.Cities.length; index++) {
-        const citiesSection = document.getElementById("CitiesSection");
-        cities.set(data.Cities[index].City , data.Cities[index].Nodes)
-        citiesSection.innerHTML +=`
+  for (let index = 0; index < data.Cities.length; index++) {
+    const citiesSection = document.getElementById("CitiesSection");
+    cities.set(data.Cities[index].City, data.Cities[index].Nodes);
+    citiesSection.innerHTML += `
         
         <option value="${data.Cities[index].City}">${data.Cities[index].City}</option>
 
-        `
-    } 
-    for (let index = 0; index < data.BasePrice.length; index++) {
-        base = data.BasePrice[index].Base;
-        KingTax = data.BasePrice[index].KingTaxes;
-        CityTax = data.BasePrice[index].CityTaxes;
-        NodeMovement = data.BasePrice[index].NodeMovement;
-       
-        Taxes = KingTax + CityTax
-      
-    }   
+        `;
+  }
+  for (let index = 0; index < data.BasePrice.length; index++) {
+    base = data.BasePrice[index].Base;
+    KingTax = data.BasePrice[index].KingTaxes;
+    CityTax = data.BasePrice[index].CityTaxes;
+    NodeMovement = data.BasePrice[index].NodeMovement;
 
+    Taxes = KingTax + CityTax;
+  }
 
   for (let index = 0; index < data.Bakery.length; index++) {
     const bakerySection = document.getElementById("BakerySection");
@@ -95,232 +80,201 @@ fetchdata().then((data) => {
     let product = data.Bakery[index].return;
     let tool = data.Bakery[index].Tool;
     let profesion = data.Bakery[index].Profesion;
-    let price = finalPrice(data.Bakery[index].Price)
-   
+    let price = finalPrice(data.Bakery[index].Price);
+
     bakerySection.innerHTML += `
  
-            <tr>
+        <tr>
+            <td>${name}</td>
+            <td>${ingredients}</td>
+            <td>${product}</td>
+            <td>${tool}</td>
+            <td>${profesion}</td>
+            <td>${price}</td>
+        
+        </tr>
+
+    `;
+  }
+  for (let index = 0; index < data.Blacksmith.length; index++) {
+    const blacksmithSection = document.getElementById("BlacksmithSection");
+    let name = data.Blacksmith[index].name;
+    let ingredients = data.Blacksmith[index].ingredients;
+    let product = data.Blacksmith[index].return;
+    let tool = data.Blacksmith[index].Tool;
+    let profesion = data.Blacksmith[index].Profesion;
+    let price = finalPrice(data.Blacksmith[index].Price);
+
+    blacksmithSection.innerHTML += `
+   
+        <tr>
                 <td>${name}</td>
                 <td>${ingredients}</td>
                 <td>${product}</td>
                 <td>${tool}</td>
                 <td>${profesion}</td>
                 <td>${price}</td>
-            
-            </tr>
-    
-      `;
+        </tr>
+
+    `;
   }
-    for (let index = 0; index < data.Blacksmith.length; index++) {
-      const blacksmithSection = document.getElementById("BlacksmithSection");      
-      let name = data.Blacksmith[index].name;
-      let ingredients = data.Blacksmith[index].ingredients;
-      let product = data.Blacksmith[index].return;
-      let tool = data.Blacksmith[index].Tool;
-      let profesion = data.Blacksmith[index].Profesion;
-      let price =finalPrice(data.Blacksmith[index].Price)
-  
-      blacksmithSection.innerHTML += `
-   
-            <tr>
-                  <td>${name}</td>
-                  <td>${ingredients}</td>
-                  <td>${product}</td>
-                  <td>${tool}</td>
-                  <td>${profesion}</td>
-                  <td>${price}</td>
-            </tr>
+  for (let index = 0; index < data.Breeder.length; index++) {
+    const breederSection = document.getElementById("BreederSection");
+    let name = data.Breeder[index].name;
+    let ingredients = data.Breeder[index].ingredients;
+    let product = data.Breeder[index].return;
+    let tool = data.Breeder[index].Tool;
+    let profesion = data.Breeder[index].Profesion;
+    let price = finalPrice(data.Breeder[index].Price);
 
-        `;
-    }  
-    for (let index = 0; index < data.Breeder.length; index++) {
-      const breederSection = document.getElementById("BreederSection");
-      let name = data.Breeder[index].name;
-      let ingredients = data.Breeder[index].ingredients;
-      let product = data.Breeder[index].return;
-      let tool = data.Breeder[index].Tool;
-      let profesion = data.Breeder[index].Profesion;
-      let price = finalPrice(data.Breeder[index].Price)
-  
-      breederSection.innerHTML += `
+    breederSection.innerHTML += `
    
-      <div class="card";">
-    
-          <div class="card-body">
-              <tr>
-                  
-                  <td>${name}</td>
-                  <td>${ingredients}</td>
-                  <td>${product}</td>
-                  <td>${tool}</td>                  
-                  <td>${profesion}</td>
-                  <td>${price}</td>
-              </tr>
-          </div>
-      </div>
-        
-        `;
-    }  
-    for (let index = 0; index < data.Carpenter.length; index++) {
-      const carpenterSection = document.getElementById("CarpenterSection");
-      let name = data.Carpenter[index].name;
-      let ingredients = data.Carpenter[index].ingredients;
-      let product = data.Carpenter[index].return;
-      let tool = data.Carpenter[index].Tool;
-      let profesion = data.Carpenter[index].Profesion;
-      let price = finalPrice(data.Carpenter[index].Price)
-  
-      carpenterSection.innerHTML += `
-   
-      <div class="card";">
-    
-          <div class="card-body">
-              <tr>
-                  
-                  <td>${name}</td>
-                  <td>${ingredients}</td>
-                  <td>${product}</td>
-                  <td>${tool}</td>                  
-                  <td>${profesion}</td>
-                  <td>${price}</td>
-              </tr>
-          </div>
-      </div>
-        
-        `;
-    }  
-
-    for (let index = 0; index < data.Farmer.length; index++) {
-      const farmerSection = document.getElementById("FarmerSection");
-      let name = data.Farmer[index].name;
-      let ingredients = data.Farmer[index].ingredients;
-      let product = data.Farmer[index].return;
-      let tool = data.Farmer[index].Tool;
-      let profesion = data.Farmer[index].Profesion;
-      let price = finalPrice(data.Farmer[index].Price)
-  
-      farmerSection.innerHTML += `
-   
-      <div class="card";">
-    
-          <div class="card-body">
-              <tr>
-                  
-                  <td>${name}</td>
-                  <td>${ingredients}</td>
-                  <td>${product}</td>
-                  <td>${tool}</td>                  
-                  <td>${profesion}</td>
-                  <td>${price}</td>
-              </tr>
-          </div>
-      </div>
-        
-        `;
-    }  
-
-    for (let index = 0; index < data.Herbalist.length; index++) {
-      const herbalistSection = document.getElementById("HerbalistSection");
-      let name = data.Herbalist[index].name;
-      let ingredients = data.Herbalist[index].ingredients;
-      let product = data.Herbalist[index].return;
-      let tool = data.Herbalist[index].Tool;
-      let profesion = data.Herbalist[index].Profesion;
-      let price = finalPrice(data.Herbalist[index].Price)
-  
-      herbalistSection.innerHTML += `
-   
-      <div class="card";">
-    
-          <div class="card-body">
-              <tr>
-                  
-                  <td>${name}</td>
-                  <td>${ingredients}</td>
-                  <td>${product}</td>
-                  <td>${tool}</td>                  
-                  <td>${profesion}</td>
-                  <td>${price}</td>
-              </tr>
-          </div>
-      </div>
-        
-        `;
-    }  
-
-    for (let index = 0; index < data.Potter.length; index++) {
-      const potterSection = document.getElementById("PotterSection");
-      let name = data.Potter[index].name;
-      let ingredients = data.Potter[index].ingredients;
-      let product = data.Potter[index].return;
-      let tool = data.Potter[index].Tool;
-      let profesion = data.Potter[index].Profesion;
-      let price = finalPrice(data.Potter[index].Price)
-  
-      potterSection.innerHTML += `
-   
-      <div class="card";">
-    
-          <div class="card-body">
-              <tr>
-                  
-                  <td>${name}</td>
-                  <td>${ingredients}</td>
-                  <td>${product}</td>
-                  <td>${tool}</td>                  
-                  <td>${profesion}</td>
-                  <td>${price}</td>
-              </tr>
-          </div>
-      </div>
-        
-        `;
-    }  
-   for (let index = 0; index < data.Tailor.length; index++) {
-      const tailorSection = document.getElementById("TailorSection");
-      let name = data.Tailor[index].name;
-      let ingredients = data.Tailor[index].ingredients;
-      let product = data.Tailor[index].return;
-      let tool = data.Tailor[index].Tool;
-      let profesion = data.Tailor[index].Profesion;
-      let price = finalPrice(data.Tailor[index].Price)
-  
-      tailorSection.innerHTML += `
-        <div>
-            <tr>
-                
+        <tr>
                 <td>${name}</td>
                 <td>${ingredients}</td>
                 <td>${product}</td>
-                <td>${tool}</td>                
+                <td>${tool}</td>                  
                 <td>${profesion}</td>
                 <td>${price}</td>
-            </tr>
-        </div>  
-        `;
-    }  
-    for (let index = 0; index < data.Materials.length; index++) {
-        const materialsSection = document.getElementById("MaterialsSection");
-        let name = data.Materials[index].name;
-        let node = data.Materials[index].node;
-        let product = data.Materials[index].return;
-        let price = finalPrice(data.Materials[index].Price)
+        </tr>
+
     
-        materialsSection.innerHTML += `
-          <div>
-              <tr>
-                  
-                  <td>${name}</td>
-                  <td>${node}</td>
-                  <td>${product}</td>
-                  <td>${price}</td>
-              </tr>
-          </div>  
-          `;
-      }  
+    `;
+  }
+  for (let index = 0; index < data.Carpenter.length; index++) {
+    const carpenterSection = document.getElementById("CarpenterSection");
+    let name = data.Carpenter[index].name;
+    let ingredients = data.Carpenter[index].ingredients;
+    let product = data.Carpenter[index].return;
+    let tool = data.Carpenter[index].Tool;
+    let profesion = data.Carpenter[index].Profesion;
+    let price = finalPrice(data.Carpenter[index].Price);
 
-}
+    carpenterSection.innerHTML += `
+   
+        <tr>
+                <td>${name}</td>
+                <td>${ingredients}</td>
+                <td>${product}</td>
+                <td>${tool}</td>                  
+                <td>${profesion}</td>
+                <td>${price}</td>
+        </tr>
 
+    
+    `;
+  }
 
-)
+  for (let index = 0; index < data.Farmer.length; index++) {
+    const farmerSection = document.getElementById("FarmerSection");
+    let name = data.Farmer[index].name;
+    let ingredients = data.Farmer[index].ingredients;
+    let product = data.Farmer[index].return;
+    let tool = data.Farmer[index].Tool;
+    let profesion = data.Farmer[index].Profesion;
+    let price = finalPrice(data.Farmer[index].Price);
 
+    farmerSection.innerHTML += `
+   
+
+        <tr>
+            <td>${name}</td>
+            <td>${ingredients}</td>
+            <td>${product}</td>
+            <td>${tool}</td>                  
+            <td>${profesion}</td>
+            <td>${price}</td>
+        </tr>
+
+        
+    `;
+  }
+
+  for (let index = 0; index < data.Herbalist.length; index++) {
+    const herbalistSection = document.getElementById("HerbalistSection");
+    let name = data.Herbalist[index].name;
+    let ingredients = data.Herbalist[index].ingredients;
+    let product = data.Herbalist[index].return;
+    let tool = data.Herbalist[index].Tool;
+    let profesion = data.Herbalist[index].Profesion;
+    let price = finalPrice(data.Herbalist[index].Price);
+
+    herbalistSection.innerHTML += `
+   
+        <tr>
+            <td>${name}</td>
+            <td>${ingredients}</td>
+            <td>${product}</td>
+            <td>${tool}</td>                  
+            <td>${profesion}</td>
+            <td>${price}</td>
+        </tr>
+
+        
+    `;
+  }
+
+  for (let index = 0; index < data.Potter.length; index++) {
+    const potterSection = document.getElementById("PotterSection");
+    let name = data.Potter[index].name;
+    let ingredients = data.Potter[index].ingredients;
+    let product = data.Potter[index].return;
+    let tool = data.Potter[index].Tool;
+    let profesion = data.Potter[index].Profesion;
+    let price = finalPrice(data.Potter[index].Price);
+
+    potterSection.innerHTML += `
+   
+        <tr>
+            <td>${name}</td>
+            <td>${ingredients}</td>
+            <td>${product}</td>
+            <td>${tool}</td>                  
+            <td>${profesion}</td>
+            <td>${price}</td>
+        </tr>
+
+    
+    `;
+  }
+  for (let index = 0; index < data.Tailor.length; index++) {
+    const tailorSection = document.getElementById("TailorSection");
+    let name = data.Tailor[index].name;
+    let ingredients = data.Tailor[index].ingredients;
+    let product = data.Tailor[index].return;
+    let tool = data.Tailor[index].Tool;
+    let profesion = data.Tailor[index].Profesion;
+    let price = finalPrice(data.Tailor[index].Price);
+
+    tailorSection.innerHTML += `
+
+        <tr>
+            <td>${name}</td>
+            <td>${ingredients}</td>
+            <td>${product}</td>
+            <td>${tool}</td>                
+            <td>${profesion}</td>
+            <td>${price}</td>
+        </tr>
+
+    `;
+  }
+  for (let index = 0; index < data.Materials.length; index++) {
+    const materialsSection = document.getElementById("MaterialsSection");
+    let name = data.Materials[index].name;
+    let node = data.Materials[index].node;
+    let product = data.Materials[index].return;
+    let price = finalPrice(data.Materials[index].Price);
+
+    materialsSection.innerHTML += `
+
+        <tr>
+            <td>${name}</td>
+            <td>${node}</td>
+            <td>${product}</td>
+            <td>${price}</td>
+        </tr>
+
+    `;
+  }
+});
